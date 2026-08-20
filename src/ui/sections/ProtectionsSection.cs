bd615f059acae65e59618354c1afe96e6c80e25f
valvicthe <louie12brett@icloud.com>
2026-08-20 15:20:56 +0100

    Refactor ProtectionsSection class and namespace

--- src/ui/sections/ProtectionsSection.cs
+++ src/ui/sections/ProtectionsSection.cs
@@ -1,7 +1,7 @@
-﻿using HydraMenu.features;
+﻿using HydroxideMenu.features;
 using UnityEngine;
 
-namespace HydraMenu.ui.sections
+namespace HydroxideMenu.ui.sections
 {
 	internal class ProtectionsSection : ISection
 	{
